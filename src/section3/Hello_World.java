0026f14d093590bc14c638c8ccb45252c8a39878
andrew983 <league@WTS14-4.attlocal.net>
2019-03-20 18:25:44 -0700

    day 3
    yay boi we called people fat

---
+++ src/section3/Hello_World.java
@@ -0,0 +1,10 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Hello_World {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	string name=JOptionPane.showInputDialog("What is your name?");
+}
+}
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,12 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+		String name=JOptionPane.showInputDialog("Are you dead?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (name.equals("no")) {	JOptionPane.showMessageDialog("Supriseing! You look pretty dead");
 		// 3. Otherwise, wish them good luck washing dishes.
-
+else (JOptionPane.showMessageDialog("Yea you look like ur dead");
+	
 	}
 }
 
