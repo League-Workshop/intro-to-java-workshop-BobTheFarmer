9349bf880faa968b78aabfa30acb3d193ccd1aa6
andrew983 <league@WTS14-4.attlocal.net>
2019-03-27 18:25:23 -0700

    Day 4
    Big Chunges and GAGAGAGAGAGAGAGAGAGAGAGAGAgAGAgAGAGAGAgAGAGGAgAGAGAGA

--- src/section3/StephenHawking.java
+++ src/section3/StephenHawking.java
@@ -7,18 +7,18 @@ import com.sun.speech.freetts.VoiceManager;
 public class StephenHawking {
 	
 	// 1. make a main method and put steps 2, 3 & 4 inside it
-	
+	public static void main(String[] args) {
 	// 4. Use a for loop to repeat steps #2 and #3, a lot of times
-	
+		for(int i=0; i<99999; i++) {
 	// 2. ask the user for a sentence
-	
+		String Sentence=JOptionPane.showInputDialog("Gimme a sentece");
 	// 3. call the speak method below and send it the sentence
-
+speak(Sentence);
 	
 
 	
-	
-	
+		}
+	}
 	
 	
 	
@@ -30,7 +30,7 @@ public class StephenHawking {
 		}
 		 catch (Exception e) {
 			 e.printStackTrace();
+		 
 		 }
-	}
-
-}
+		
+	
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -8,10 +8,11 @@ public class WorldDomination {
 		// 1. Ask the user if they know how to write code.
 		String name=JOptionPane.showInputDialog("Are you dead?");
 		// 2. If they say "yes", tell them they will rule the world.
-if (name.equals("no")) {	JOptionPane.showMessageDialog("Supriseing! You look pretty dead");
+if (name.equals("no")) {	JOptionPane.showMessageDialog(null, "Supriseing! You look pretty dead");
 		// 3. Otherwise, wish them good luck washing dishes.
-else (JOptionPane.showMessageDialog("Yea you look like ur dead");
+} else { JOptionPane.showMessageDialog(null, "Yea you look like ur dead");
 	
 	}
 }
 
+}
